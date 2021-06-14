31281e36458d3ab28321f41d4d905b2bf8624bff
scortier <onlytoaditya@gmail.com>
2021-06-14 13:45:52 +0530

    working on the code

--- src/main/board.java
+++ src/main/board.java
@@ -1,5 +1,13 @@
 // Each cell is encouter by its surrounding 8 cells
-//alive  0 -> 3 life neighbours -> 1  
+//alive  0 -> exact  3 life neighbours -> 1  
 //death 1 -> <2 life or  >3 life -> 0
-// next gen 1 -> 2 or 3 life -> next gen 
+// next gen 1 -> 2 or 3 life -> 1 
 //2d array which store random collection of 0 and 1
+
+
+class board{
+    public void GOT(int [][] board)
+    {
+        
+    }
+}
