3adf4238ae85ae1f6412d244f47952f2579cd466
scortier <onlytoaditya@gmail.com>
2021-06-14 13:17:13 +0530

    breaking down the statement

---
+++ src/main/board.java
@@ -0,0 +1,4 @@
+// Each cell is encouter by its surrounding 8 cells
+//alive  0 -> 3 life neighbours -> 1  
+//death 1 -> <2 life or  >3 life -> 0
+//2d array which store random collection of 0 and 1
